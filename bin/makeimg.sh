561bd185604e23d6f12a48ecb38668bc914381dc
anqiu1987 <anqiu1987@sina.com>
2013-05-14 22:33:14 +0800

    add

--- bin/makeimg.sh
+++ bin/makeimg.sh
@@ -3,5 +3,19 @@ if [ -f "bootloader.bin" ];then
 dd if=bootloader.bin of=disk.img bs=512 count=1
 else
 echo "bootloaer.bin not exist"
+exit
+fi
+if [ -f "setup.bin" ]; then
+dd if=setup.bin of=disk.img bs=512 seek=1
+else
+echo "setup.bin not exist"
+exit
+fi
+
+if [ -f "kernel"]; then
+dd if=setup.bin of=disk.img bs=512 seek=5
+else
+echo "kernel not exist"
+exit
 fi
 
