f038c27764916efee8c3cdd3d219b33772cc374d
jintao.ju <644104436@qq.com>
2013-05-31 17:19:03 +0800

    add trap

--- trap.c
+++ trap.c
@@ -113,6 +113,7 @@ void trap_init()
 	set_system_gate(16, & coprocessor_error);
 	for(i=17; i < 48; i++)
 		set_trap_gate(i, &reserved);
-	set_trap_gate(45, &irq13);
+	set_trap_gate(45, &irq13);//Ð­´¦ÀíÆ÷
+	
 	
 }
